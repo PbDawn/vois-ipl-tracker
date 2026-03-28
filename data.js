9c2330bca8d71c3e3ef209151c7fe7a94188be4b
PbDawn <93029548+PbDawn@users.noreply.github.com>
2026-03-29 02:31:08 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,65 @@
+window.IPL_MATCH_DATA = [
+  {
+    "matchno": 1,
+    "date": "2026-03-28",
+    "teams": "RCB vs SRH",
+    "teamwon": "RCB",
+    "contest": "yes",
+    "fee": 50,
+    "joinedCount": 7,
+    "paidCount": 7,
+    "pool": 350,
+    "fantasyWinner": "Prabhat",
+    "tiedWinners": [
+      "Prabhat"
+    ],
+    "winningPerWinner": 350,
+    "players": {
+      "Ashish": {
+        "joined": true,
+        "paid": true,
+        "points": 657.5
+      },
+      "Kalpesh": {
+        "joined": true,
+        "paid": true,
+        "points": 1048
+      },
+      "Nilesh": {
+        "joined": true,
+        "paid": true,
+        "points": 832.5
+      },
+      "Prabhat": {
+        "joined": true,
+        "paid": true,
+        "points": 1081.5
+      },
+      "Pritam": {
+        "joined": true,
+        "paid": true,
+        "points": 919.5
+      },
+      "Sudhir": {
+        "joined": true,
+        "paid": true,
+        "points": 971.5
+      },
+      "Swapnil": {
+        "joined": true,
+        "paid": true,
+        "points": 844.5
+      }
+    },
+    "joinedRanks": {
+      "Prabhat": 1,
+      "Kalpesh": 2,
+      "Sudhir": 3,
+      "Pritam": 4,
+      "Swapnil": 5,
+      "Nilesh": 6,
+      "Ashish": 7
+    },
+    "transferred": false
+  }
+];
